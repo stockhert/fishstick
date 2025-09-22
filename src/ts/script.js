18deb1b67217936732bb67ae8753a41d0f124d28
stockhert <harrytoshi@gmail.com>
2025-09-22 13:09:55 +0200

    merged main to script. added code background effect to index. amended css

--- src/ts/script.js
+++ src/ts/script.js
@@ -3,3 +3,17 @@ function showByID(formID) {
     document.querySelectorAll(".loginContainer").forEach(function (form) { return form.classList.remove("active"); });
     (_a = document.getElementById(formID)) === null || _a === void 0 ? void 0 : _a.classList.add("active");
 }
+function typeOut(element, delay) {
+    if (delay === void 0) { delay = 100; }
+    var i = 0;
+    var text = element.textContent;
+    element.textContent = "";
+    function typeCharacter() {
+        if (i < text.length) {
+            element.textContent += text[i];
+            i++;
+            setTimeout(typeCharacter, delay);
+        }
+    }
+    typeCharacter();
+}
--- src/ts/script.ts
+++ src/ts/script.ts
@@ -2,3 +2,19 @@ function showByID(formID: string): void {
     document.querySelectorAll<HTMLElement>(".loginContainer").forEach(form => form.classList.remove("active"));
     document.getElementById(formID)?.classList.add("active");
 }
+
+function typeOut(element:HTMLElement, delay:number = 100){
+
+    let i: number = 0
+    let text: string = element.textContent;
+    element.textContent = ""
+
+    function typeCharacter(){
+        if(i < text.length){
+            element.textContent += text[i];
+            i++
+            setTimeout(typeCharacter, delay)
+        }
+    }
+    typeCharacter()
+}
