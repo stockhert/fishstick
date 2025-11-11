f79e334c82ba5fbc48bfa69e437518ecb734c7a9
stockhert <harrytoshi@gmail.com>
2025-11-11 09:13:40 +0100

    small changes

--- src/ts/script.ts
+++ src/ts/script.ts
@@ -2,6 +2,7 @@
 import {type} from "node:os";
 import data from 'src/Lessons.json' assert type( type: json) //uhguhghghhhhhhh
 
+let toggled = false //used for toggler
 
 function showByID(targetID: string, HTMLElementClass: string): void {
     document.querySelectorAll<HTMLElement>(HTMLElementClass).forEach(element => element.classList.remove("active"))
@@ -23,6 +24,13 @@ function typeOut(element:HTMLElement, delay:number = 100){
         }
     }
     typeCharacter()
+}
+
+function aboutToggler(targetElement:string, HTMLElementClass:string, defaultClass:string, targetClass:string){
+    toggled = !toggled
+    if(toggled){
+
+    }
 }
 
 // lessons
